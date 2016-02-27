7c6161ca187fb512ff2a0bbf938fbab1a922e151
Markus Schmidt <m.schmidt@acemail.de>
2016-02-27 22:31:19 +0100

    Implemented loader

--- index.ts
+++ index.ts
@@ -1,7 +1,65 @@
+const fs = require('fs');
+
 module.exports = function(content) {
-	var js = `var Template = require('access-core/app/template')`;
+	this.cacheable();
 	
-	console.log(content);
+    var resolve = this.resolve.bind(this),
+		context = this.context,
+		callback = this.async(),
+		components = [],
+		attributes = [],
+		waiting = 0;
 	
-    return js;
+	content = content
+		.replace(/\s+/g, ' ')
+		.replace(/'/g, "\\'")
+		.replace(/<use\s+(component|attribute)="(.*?)"\s*(as="(.*?)"\s*)?\/>\s*/ig, function(m, kind, path, _, as) {
+			waiting++;
+			
+			resolve(context, path, function(err, use) {
+				if (err) {
+					return callback(err);
+				}
+				
+				var desc = [];
+				
+				if (kind === 'component') {
+					if (fs.existsSync(use)) {
+						desc.push(`Component: require('${path}')['default']`);
+					}
+					
+					if (fs.existsSync(use.replace(/js$/, 'html'))) {
+						desc.push(`template: require('${path}.html')['default']`);
+					}
+					
+					if (desc.length) {
+						as = as || path.split('/').pop();
+						components.push(`'${as.toUpperCase()}': {${desc.join(', ')}}`);
+					}
+				} else if (fs.existsSync(use)) {
+					as = as || path.split('/').pop();
+					attributes.push(`'${as}': require('${path}')['default']`);
+				}
+				
+				if (!--waiting) {
+					finish();
+				}
+			});
+		
+		return '';
+	});	
+
+	function finish() {
+		callback(null, `"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+	
+var Template = require('access-core/app/template')['default'],
+	components = {${components.join(', ')}},
+	attributes = {${attributes.join(', ')}};		
+
+exports.default = new Template('${content}', components, attributes);`);
+	}
 };
