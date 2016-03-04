b33fac5314b37575f3b208d55d8641897a9f5565
Markus Schmidt <m.schmidt@acemail.de>
2016-03-04 18:03:48 +0100

    Made sure path resolving is async

--- index.ts
+++ index.ts
@@ -1,53 +1,55 @@
 const fs = require('fs');
 
 module.exports = function(content) {
-	this.cacheable();
-	
-    var resolve = this.resolve.bind(this),
+	var resolve = this.resolve.bind(this),
 		context = this.context,
 		callback = this.async(),
 		components = [],
 		attributes = [],
 		waiting = 0;
 	
+	this.cacheable();
+	
 	content = content
 		.replace(/\s+/g, ' ')
 		.replace(/'/g, "\\'")
-		.replace(/<use\s+(component|attribute)="(.*?)"\s*(as="(.*?)"\s*)?\/>\s*/ig, function(m, kind, path, _, as) {
+		.replace(/<use\s(component|attribute)="(.*?)"(\s?as="(.*?)")?\s?\/>\s?/ig, function(m, kind, path, _, as) {
 			waiting++;
 			
-			resolve(context, path, function(err, use) {
-				if (err) {
-					return callback(err);
-				}
-				
-				var desc = [];
-				
-				if (kind === 'component') {
-					if (fs.existsSync(use)) {
-						desc.push(`Component: require('${path}')['default']`);
+			process.nextTick(function() {
+				resolve(context, path, function(err, use) {
+					if (err) {
+						return callback(err);
 					}
 					
-					if (fs.existsSync(use.replace(/js$/, 'html'))) {
-						desc.push(`template: require('${path}.html')['default']`);
-					}
+					var desc = [];
 					
-					if (desc.length) {
+					if (kind === 'component') {
+						if (fs.existsSync(use)) {
+							desc.push(`Component: require('${path}')['default']`);
+						}
+						
+						if (fs.existsSync(use.replace(/js$/, 'html'))) {
+							desc.push(`template: require('${path}.html')['default']`);
+						}
+						
+						if (desc.length) {
+							as = as || path.split('/').pop();
+							components.push(`'${as.toUpperCase()}': {${desc.join(', ')}}`);
+						}
+					} else if (fs.existsSync(use)) {
 						as = as || path.split('/').pop();
-						components.push(`'${as.toUpperCase()}': {${desc.join(', ')}}`);
+						attributes.push(`'${as}': require('${path}')['default']`);
 					}
-				} else if (fs.existsSync(use)) {
-					as = as || path.split('/').pop();
-					attributes.push(`'${as}': require('${path}')['default']`);
-				}
-				
-				if (!--waiting) {
-					finish();
-				}
+					
+					if (!--waiting) {
+						finish();
+					}
+				});
 			});
-		
-		return '';
-	});	
+			
+			return '';
+		});
 
 	function finish() {
 		callback(null, `"use strict";
