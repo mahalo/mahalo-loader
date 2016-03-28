ad2db8ab6ff1f77c75ade535671fd4a8ab19c146
Markus Schmidt <m.schmidt@acemail.de>
2016-03-28 22:09:11 +0200

    Added real minifying and optimized resolving

--- index.ts
+++ index.ts
@@ -1,93 +1,145 @@
-import fs from 'fs';
+import {minify} from 'html-minifier';
+
+var config = {
+		keepClosingSlash: true,
+		collapseWhitespace:	true,
+		conservativeCollapse: true
+	};
 
 export default function mahaloLoader(content) {
 	var context = this.context,
 		callback = this.async(),
 		uses = [],
-		components = [],
-		behaviors = [],
+		_components = [],
+		_behaviors = [],
 		waiting = 0;
 	
 	this.cacheable(true);
 	
-	content = content
-		.replace(/\s+/g, ' ')
-		.replace(/'/g, "\\'")
-		.replace(/<use\s(component|behavior)="(.*?)"(\s?as="(.*?)")?\s?\/>\s?/ig, function(m, kind, path, _, as) {
-			if (!callback) {
-				checkFiles(kind, path, as, this.resolveSync(context, path));
-			} else {
-				waiting++;
-				
-				uses.push([kind, path, as]);
-			}
+	content = minify(content, config).replace(/<use\s(component|behavior)="(.*?)"(\s?as="(.*?)")?\s?\/>\s?/ig, (m, kind, path, _, as) => {
+		if (!callback) {
+			resolveSync.call(this, kind, path, as);
+		} else {
+			waiting++;
+			kind === 'component' && waiting++;
 			
-			return '';
-		}.bind(this));
-	
-	if (callback) {
-		if (!waiting) {
-			return finish() || '';
+			uses.push([kind, path, as]);
 		}
 		
-		uses.forEach(resolveAsync, this);
-	} else {
+		return '';
+	});
+	
+	if (!callback) {
 		return render();
 	}
 	
+	return (uses.length ? uses.forEach(resolveAsync, this) : finish()) || '';
+	
 	function resolveAsync(item) {
-		var kind = item[0],
-			path = item[1],
-			as = item[2];
+		var path = item[1],
+			desc = {
+				as: as(item[2], path),
+				path: path,
+				files: []
+			};
 		
-		this.resolve(context, path, function(err, use) {
-			if (err) {
-				return callback(err);
-			}
+		if (item[0] === 'component') {
+			_components.push(desc);		
 			
-			checkFiles(kind, path, as, use);
+			this.resolve(context, path, addComponent.bind(null, desc));
+			this.resolve(context, path + '.html', addTemplate.bind(null, desc));
+		} else {
+			_behaviors.push(desc);
 			
-			--waiting || finish();
-		});
-	}
-		
-	function checkFiles(kind, path, as, use) {
-		var desc = [];
-		
-		if (kind === 'component') {
-			if (fs.existsSync(use)) {
-				desc.push(`Component: require('${path}')['default']`);
-			}
-			
-			if (fs.existsSync(use.replace(/(ts|js)$/, 'html'))) {
-				desc.push(`template: require('${path}.html')['default']`);
-			}
-			
-			if (desc.length) {
-				as = as || path.split('/').pop();
-				components.push(`'${as.toUpperCase()}': {${desc.join(', ')}}`);
-			}
-		} else if (fs.existsSync(use)) {
-			as = as || path.split('/').pop();
-			behaviors.push(`'${as}': require('${path}')['default']`);
+			this.resolve(context, path, addBehavior.bind(this, desc));
 		}
 	}
 	
+	function resolveSync(kind, path, as) {
+		var desc = {
+				as: as(as, path),
+				path: path,
+				files: []
+			};
+		
+		if (kind === 'component') {
+			_components.push(desc);		
+			
+			this.resolveSync(context, path) && addComponent(desc);
+			this.resolveSync(context, path + '.html') && addTemplate(desc);
+		} else {
+			_behaviors.push(desc);
+			
+			this.resolveSync(context, path) && addBehavior(desc);
+		}
+	}
+	
+	function addComponent(desc, err?) {
+		if (!err) {
+			desc.files.push(`Component: require('${desc.path}')['default']`);
+		}
+		
+		finish();
+	}
+	
+	function addTemplate(desc, err?) {
+		if (!err) {
+			desc.files.push(`template: require('${desc.path}.html')['default']`);
+		}
+		
+		finish();
+	}
+	
+	function addBehavior(desc, err?) {
+		if (!err) {
+			desc.files.push(`'${desc.as}': require('${desc.path}')['default']`);
+		}
+		
+		finish();
+	}
+	
+	function as(as: string, path: string) {
+		return (as || path.split('/').pop()).toUpperCase();
+	}
+	
 	function finish() {
-		callback(null, render());
+		if (--waiting > 0) {
+			return;
+		}
+		
+		callback && callback(null, render());
 	}
 
 	function render() {
+		var components = [],
+			behaviors = [];
+		
+		_components.forEach(component => {
+			if (!component.files.length) {
+				return;
+			}
+			
+			components.push(`'${component.as}': {\n\t\t${component.files.join(', ')}\n\t}`);
+		});
+		
+		_behaviors.forEach(behavior => {
+			if (!behavior.files.length) {
+				return;
+			}
+			
+			behaviors.push(behavior.files[0]);
+		});
+		
 		return `"use strict";
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-	
+
 var Template = require('mahalo/mahalo')['Template'],
-	components = {${components.join(', ')}},
+	components = {\n\t${components.join(',\n\t')}\n},
 	behaviors = {${behaviors.join(', ')}};		
 
-exports.default = new Template('${content}', components, behaviors);`;
+exports.default = new Template(${JSON.stringify(content)}, components, behaviors);`;
 	}
 };
